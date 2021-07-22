8c1b80f1dbceca96d21709cd325dcd1a0aea5cc2
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2021-07-22 10:34:21 -0600

    add port property to server.listen

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const server = new ApolloServer({
   },
 });
 
-server.listen().then(() => {
+server.listen({ port: process.env.PORT || 4000 }).then(() => {
   console.log(`
     🚀  Server is running!
     🔉  Listening on port 4000
