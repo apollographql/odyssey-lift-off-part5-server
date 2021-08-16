d5a80afff154ed018382f6dbab1690ca30e34f20
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2021-08-16 15:22:50 -0400

    switch to async function for server start instead of callback

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
-
 const TrackAPI = require('./datasources/track-api');
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI(),
-    };
-  },
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI(),
+      };
+    },
+  });
 
-server.listen().then(() => {
+  const { url, port } = await server.listen();
   console.log(`
-    🚀  Server is running!
-    🔉  Listening on port 4000
-    📭  Query at https://studio.apollographql.com/dev
-  `);
-});
+      🚀  Server is running
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer(typeDefs, resolvers);
