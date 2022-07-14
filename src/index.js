bfbc66724b2ed55faf45ea0d1dee8b21b4827ffb
Kate Papineni <kate.papineni@gmail.com>
2022-07-13 19:50:49 -0500

    Update server to listen for port from env variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
-const TrackAPI = require('./datasources/track-api');
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api");
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
@@ -14,7 +14,7 @@ async function startApolloServer(typeDefs, resolvers) {
     },
   });
 
-  const { url, port } = await server.listen();
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
   console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
