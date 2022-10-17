0b61b6fb63a02d1be2a161ef167b6bfea9282452
Liz Hennessy <eleighhennessy@gmail.com>
2022-10-17 10:17:04 -0600

    make changes to update to as4

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone')
+
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
 
-async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => {
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
       return {
-        trackAPI: new TrackAPI(),
-      };
+        dataSources: {
+          trackAPI: new TrackAPI()
+        }
+      }
     },
-  });
+    listen: { port: process.env.PORT || 4000 }
+  })
 
-  const { url, port } = await server.listen();
   console.log(`
       🚀  Server is running
-      🔉  Listening on port ${port}
       📭  Query at ${url}
     `);
 }
 
-startApolloServer(typeDefs, resolvers);
+startApolloServer();
