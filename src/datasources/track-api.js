811774f3a743c221bd1f594a662e2102929e3569
Liz Hennessy <lizhennessy@Lizs-MBP.hsd1.ut.comcast.net>
2023-04-05 10:21:37 -0600

    update constructor

--- src/datasources/track-api.js
+++ src/datasources/track-api.js
@@ -1,14 +1,11 @@
-const { RESTDataSource } = require('@apollo/datasource-rest');
+const { RESTDataSource } = require("@apollo/datasource-rest");
 
 class TrackAPI extends RESTDataSource {
-  constructor() {
-    super();
-    // the Catstronauts catalog is hosted on this server
-    this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
-  }
+  // the Catstronauts catalog is hosted on this server
+  baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
 
   getTracksForHome() {
-    return this.get('tracks');
+    return this.get("tracks");
   }
 
   getAuthor(authorId) {
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const { ApolloServer } = require('@apollo/server');
-const { startStandaloneServer } = require('@apollo/server/standalone');
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
 
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
 
-const TrackAPI = require('./datasources/track-api');
+const TrackAPI = require("./datasources/track-api");
 
 async function startApolloServer() {
   const server = new ApolloServer({ typeDefs, resolvers });
 
   const { url } = await startStandaloneServer(server, {
-    const { cache } = server;
     context: async () => {
+      const { cache } = server;
       return {
         dataSources: {
           trackAPI: new TrackAPI({ cache }),
